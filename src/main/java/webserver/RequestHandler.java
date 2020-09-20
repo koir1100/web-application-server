fe4ba8a0aa619d15acf844bf3cbd6efd7e0d5714
dragonlinux <dragonlinux@DragonLinux>
2020-09-21 00:35:50 +0900

    3장 요구사항 1 수행

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -2,11 +2,13 @@ package webserver;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.file.Files;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -28,26 +30,33 @@ public class RequestHandler extends Thread {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
         	  BufferedReader br = new BufferedReader(new InputStreamReader(in));
         	  String line = br.readLine();
+    		  if(line == null) return;
+        	  String[] tokens = line.split(" ");
+        	  String url = tokens[1];
         	  
         	  while(!"".equals(line)) {
-        		  if(line == null) return;
         		  System.out.println(line);
         		  line = br.readLine();
         	  }
         	  
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
-            response200Header(dos, body.length);
+            byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
+            response200Header(dos, url, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
             log.error(e.getMessage());
         }
     }
 
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
+    private void response200Header(DataOutputStream dos, String url, int lengthOfBodyContent) {
+    	String[] splitDot = url.split("\\.");
+    	String ext = splitDot[splitDot.length-1];
+    	String type = "text/html";
+    	
+    	if(ext.equals("css")) type = "text/css";
         try {
             dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+            dos.writeBytes("Content-Type: " + type + ";charset=utf-8\r\n");
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
             dos.writeBytes("\r\n");
         } catch (IOException e) {
